ee272ceb7e639ecae4223f57057c59f0c4c2aace
Tom Ramsden <ramsdentom@gmail.com>
2020-09-23 22:56:19 +0100

    Implement stages-screen

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
 import styles from "./App.scss";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import WelcomeScreen from "./screens/welcome-screen";
 import TeamsScreen from "./screens/teams-screen";
+import StagesScreen from "./screens/stages-screen";
 
 function App() {
   return (
     <Router>
       <div className={styles.app}>
         <Switch>
+          <Route path="/team/:team/:stage">
+            <StagesScreen />
+          </Route>
           <Route path="/teams">
             <TeamsScreen />
           </Route>
