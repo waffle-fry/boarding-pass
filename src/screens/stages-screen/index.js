19260e8f9a2e7c10039253db962f810778295966
Tom Ramsden <ramsdentom@gmail.com>
2020-09-23 22:55:43 +0100

    Add stages-screen

---
+++ src/screens/stages-screen/index.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect, useCallback } from "react";
+import ActionButton from "../../components/action-button";
+import Header from "../../components/header";
+import data from "../../data.json";
+import styles from "./styles.scss";
+import { useParams } from "react-router-dom";
+import StepsList from "../../components/steps-list";
+
+function StagesScreen() {
+  const [state, setState] = useState(null);
+
+  useEffect(() => {
+    setState(data);
+  });
+
+  if (state == null) {
+    return "Loading...";
+  }
+
+  // let { team, stage } = useParams();
+  let team = "quantum-pipes";
+  let stage = 1;
+  let teamDetails = state.teams.find((teamDetail) => teamDetail.slug == team);
+  let currentStage = teamDetails.stages[stage - 1];
+  return (
+    <div className={styles.container}>
+      <Header
+        logo={state.logo}
+        title={teamDetails.name}
+        subtitle={currentStage.title}
+      />
+      <StepsList steps={currentStage.steps} />
+    </div>
+  );
+}
+
+export default StagesScreen;
