b642c662ffa6500ae8ee9cfa8a65b39e8c067870
Tom Ramsden <ramsdentom@gmail.com>
2020-09-24 00:07:12 +0100

    Make FontAwesome icon a prop

--- src/components/steps-list/step/index.js
+++ src/components/steps-list/step/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import styles from "./styles.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTerminal } from "@fortawesome/free-solid-svg-icons";
 import SecondaryButton from "../../secondary-button";
 
 function Step(props) {
@@ -11,7 +10,7 @@ function Step(props) {
         <div className={styles.stage_number}>{props.number}</div>
         <div className={styles.container}>
           <div className={styles.icon_container}>
-            <FontAwesomeIcon icon={faTerminal} className={styles.icon} />
+            <FontAwesomeIcon icon={props.icon} className={styles.icon} />
           </div>
           <div className={styles.content_container}>
             <div className={styles.title}>{props.title}</div>
--- src/components/steps-list/step/terminal/index.js
+++ src/components/steps-list/step/terminal/index.js
@@ -2,9 +2,16 @@ import React from "react";
 import styles from "./styles.scss";
 import Step from "..";
 import Terminal from "./terminal";
+import { faTerminal } from "@fortawesome/free-solid-svg-icons";
 
 function TerminalStep(props) {
-  return <Step {...props} additional_view={<Terminal {...props} />} />;
+  return (
+    <Step
+      {...props}
+      icon={faTerminal}
+      additional_view={<Terminal {...props} />}
+    />
+  );
 }
 
 export default TerminalStep;
