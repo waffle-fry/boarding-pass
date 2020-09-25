f614b6e5c9abae565429912667ee1c108c476b37
Tom Ramsden <ramsdentom@gmail.com>
2020-09-25 01:32:20 +0100

    Implement terminal functionality: running commands

--- src/components/steps-list/step/terminal/index.js
+++ src/components/steps-list/step/terminal/index.js
@@ -1,21 +1,77 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./styles.scss";
 import Step from "..";
 import Terminal from "./terminal";
 import { faTerminal } from "@fortawesome/free-solid-svg-icons";
+import { exec, spawn } from "child_process";
 
 function TerminalStep(props) {
   const [actionRunning, setActionRunning] = useState(false);
+  const [commands, setCommands] = useState([]);
+
+  useEffect(() => {
+    let commands = [];
+
+    props.terminal.commands.forEach(({ title, command }) => {
+      commands.push({
+        title: title,
+        command: command,
+        running: false,
+        success: false,
+        error: false,
+      });
+    });
+
+    setCommands(commands);
+  }, []);
+
+  function setCommand(index, action, value) {
+    let newCommands = commands;
+    if (action != "running") {
+      newCommands[index].running = false;
+
+      if (index + 1 == commands.length) {
+        setActionRunning(false);
+      }
+
+      // TO-DO
+      // FIX HACK: (setting commands to an empty array forces a re-render in Terminal component)
+      setCommands([]);
+    }
+
+    newCommands[index][action] = value;
+    setCommands(newCommands);
+  }
 
   function handleActionButton() {
     setActionRunning(true);
+
+    commands.forEach(({ command }, index) => {
+      setCommand(index, "running", true);
+
+      exec(command, (error, stdout, stderr) => {
+        if (error) {
+          setCommand(index, "error", true);
+          console.log(`error: ${error.message}`);
+          return;
+        }
+        if (stderr) {
+          setCommand(index, "error", true);
+          console.log(`stderr: ${stderr}`);
+          return;
+        }
+
+        setCommand(index, "success", true);
+        console.log(`stdout: ${stdout}`);
+      });
+    });
   }
 
   return (
     <Step
       {...props}
       icon={faTerminal}
-      additional_view={<Terminal {...props.terminal} />}
+      additional_view={<Terminal commands={commands} />}
       action_running={actionRunning}
       handle_action_button={handleActionButton}
     />
--- src/components/steps-list/step/terminal/terminal.js
+++ src/components/steps-list/step/terminal/terminal.js
@@ -1,3 +1,5 @@
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import styles from "./styles.scss";
 
@@ -5,12 +7,29 @@ function Terminal(props) {
   return (
     <div className={styles.terminal}>
       <div className={styles.commands}>
-        {props.commands.map((command) => (
-          <div
-            key={`command-${command}`}
-            className={styles.command}
-          >{`${command.title}: ${command.command}`}</div>
-        ))}
+        {props.commands.map(
+          ({ title, command, running, success, error }, index) => (
+            <div
+              key={`command-${index}`}
+              className={
+                success
+                  ? styles.command_success
+                  : error
+                  ? styles.command_error
+                  : styles.command
+              }
+            >
+              {running && (
+                <FontAwesomeIcon
+                  icon={faSpinner}
+                  className={styles.running_spinner}
+                  spin
+                />
+              )}
+              {`${title}: ${command}`}
+            </div>
+          )
+        )}
       </div>
     </div>
   );
