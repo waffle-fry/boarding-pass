1cf0b297851f23442a415099c4e4498f074647a2
Tom Ramsden <ramsdentom@gmail.com>
2020-09-28 16:56:29 +0100

    Fix unit test

--- src/screens/teams-screen/index.test.js
+++ src/screens/teams-screen/index.test.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { getByAltText, render } from "@testing-library/react";
 import DepartmentsScreen from "./index";
+import { BrowserRouter as Router } from "react-router-dom";
 
 test("renders header", () => {
-  const { getByAltText, getByText } = render(<DepartmentsScreen />);
+  const { getByAltText, getByText } = render(
+    <Router>
+      <DepartmentsScreen />
+    </Router>
+  );
   const logo = getByAltText("logo");
-  const title = getByText("Choose your department");
+  const title = getByText("Choose your team");
 
   expect(logo).toBeInTheDocument();
   expect(title).toBeInTheDocument();
