abd67cd00bb0bad9cdd643fd88d3d046e1c78621
Tom Ramsden <ramsdentom@gmail.com>
2020-10-07 22:57:09 +0100

    Swap out external config for internal data file

--- src/App.js
+++ src/App.js
@@ -8,23 +8,25 @@ import useAxios from "axios-hooks";
 import LoadingScreen from "./screens/loading-screen";
 
 function App() {
-  const [{ data, loading, error }, refetch] = useAxios("");
+  // const [{ data, loading, error }, refetch] = useAxios(
+  //   "https://raw.githubusercontent.com/waffle-fry/boarding-pass/master/src/data.json?token=AMCP4J3K4D4OTAMBVEFJ4KK7PUH3E"
+  // );
 
-  if (
-    loading ||
-    error ||
-    !(data !== undefined && data !== null && data.constructor == Object)
-  ) {
-    const configMalformed = !loading && !error;
+  // if (
+  //   loading ||
+  //   error ||
+  //   !(data !== undefined && data !== null && data.constructor == Object)
+  // ) {
+  //   const configMalformed = !loading && !error;
 
-    return (
-      <LoadingScreen
-        error={error}
-        retry={refetch}
-        config_malformed={configMalformed}
-      />
-    );
-  }
+  //   return (
+  //     <LoadingScreen
+  //       error={error}
+  //       retry={refetch}
+  //       config_malformed={configMalformed}
+  //     />
+  //   );
+  // }
 
   return (
     <AppContext.Provider value={data}>
