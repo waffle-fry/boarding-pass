bf69dd25f772aaa8d946bdb7b3192fa09d021aae
Tom Ramsden <ramsdentom@gmail.com>
2020-10-07 01:19:22 +0100

    Style back arrow container

--- src/components/header/back-button/index.js
+++ src/components/header/back-button/index.js
@@ -2,6 +2,7 @@ import { faLongArrowAltLeft } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { useHistory } from "react-router-dom";
+import styles from "./styles.scss";
 
 function BackButton() {
   const history = useHistory();
@@ -10,7 +11,7 @@ function BackButton() {
   };
 
   return (
-    <div className="container" onClick={goBack}>
+    <div className={styles.container} onClick={goBack}>
       <FontAwesomeIcon icon={faLongArrowAltLeft} className="arrow" size="2x" />
     </div>
   );
