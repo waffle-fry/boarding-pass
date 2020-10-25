921469d9301a8b63a05519453764e2f2390100d1
Tom Ramsden <ramsdentom@gmail.com>
2020-10-25 19:44:31 +0000

    Fix CORS errors

--- backend/server.go
+++ backend/server.go
@@ -23,12 +23,27 @@ func NewServer(config Config) *Server {
 	s := new(Server)
 
 	router := mux.NewRouter().StrictSlash(true)
+	router.Use(accessControlMiddleware)
 	router.HandleFunc("/config", s.getConfig).Methods(http.MethodGet)
 
 	s.Handler = router
 	s.config = config
 
 	return s
+}
+
+func accessControlMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS,PUT")
+		w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type")
+
+		if r.Method == "OPTIONS" {
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
 
 func (s *Server) getConfig(w http.ResponseWriter, r *http.Request) {
