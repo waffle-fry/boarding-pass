c6de21bd571f9722f28b6224cc7166a8aebb6d4f
Tom Ramsden <ramsdentom@gmail.com>
2020-10-25 19:44:17 +0000

    Add json and mapstructure annotations

--- backend/config.go
+++ backend/config.go
@@ -2,58 +2,58 @@ package main
 
 // Config - the configuration
 type Config struct {
-	Logo            string
-	CompanyName     string
-	EmailAddress    string
-	WelcomeTitle    string
-	WelcomeSubtitle string
-	PrimaryColour   string
-	SecondaryColour string
-	Teams           []Team
+	Logo            string `json:"logo" mapstructure:"logo"`
+	CompanyName     string `json:"company_name" mapstructure:"company_name"`
+	EmailAddress    string `json:"email_address" mapstructure:"email_address"`
+	WelcomeTitle    string `json:"welcome_title" mapstructure:"welcome_title"`
+	WelcomeSubtitle string `json:"welcome_subtitle" mapstructure:"welcome_subtitle"`
+	PrimaryColour   string `json:"primary_colour" mapstructure:"primary_colour"`
+	SecondaryColour string `json:"secondary_colour" mapstructure:"secondary_colour"`
+	Teams           []Team `json:"teams" mapstructure:"teams"`
 }
 
 // Team - the elements that make up a Team
 type Team struct {
-	Name   string
-	Slug   string
-	Stages []Stage
+	Name   string  `json:"name" mapstructure:"name"`
+	Slug   string  `json:"slug" mapstructure:"slug"`
+	Stages []Stage `json:"stages" mapstructure:"stages"`
 }
 
 // Stage - the elements that make up a Stage
 type Stage struct {
-	Title   string
-	Message Message
-	Steps   []Step
+	Title   string   `json:"title" mapstructure:"title"`
+	Message *Message `json:"message,omitempty" mapstructure:"message"`
+	Steps   []Step   `json:"steps" mapstructure:"steps"`
 }
 
 // Message - the elements that make up a Message
 type Message struct {
-	title string
-	text  string
+	Title string `json:"title" mapstructure:"title"`
+	Text  string `json:"text" mapstructure:"text"`
 }
 
 // Step - the elements that make up a Step
 type Step struct {
-	Type     string
-	Title    string
-	Text     string
-	Terminal Terminal
+	Type         string       `json:"type" mapstructure:"type"`
+	Title        string       `json:"title" mapstructure:"title"`
+	Text         string       `json:"text" mapstructure:"text"`
+	Terminal     *Terminal    `json:"terminal,omitempty" mapstructure:"terminal"`
+	ActionButton ActionButton `json:"action_button" mapstructure:"action_button"`
 }
 
 // Terminal - the elements that make up a Terminal
 type Terminal struct {
-	Commands     []TerminalCommand
-	ActionButton ActionButton
+	Commands []TerminalCommand `json:"commands" mapstructure:"commands"`
 }
 
 // TerminalCommand - the elements that make up a TerminalCommand
 type TerminalCommand struct {
-	Title   string
-	Command string
+	Title   string `json:"title" mapstructure:"title"`
+	Command string `json:"command" mapstructure:"command"`
 }
 
 // ActionButton - the elements that make up an ActionButton
 type ActionButton struct {
-	enabled bool
-	title   string
+	Enabled bool   `json:"enabled" mapstructure:"enabled"`
+	Title   string `json:"title" mapstructure:"title"`
 }
