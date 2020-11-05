08e1814c3c66d15330f6a499c9ab16ffa478ac1b
Tom Ramsden <ramsdentom@gmail.com>
2020-11-05 02:48:41 +0000

    Fix linting errors

--- backend/helpers_test.go
+++ backend/helpers_test.go
@@ -77,17 +77,6 @@ func getConfigFromResponse(t *testing.T, body io.Reader) (config Config) {
 	return
 }
 
-func getAppsFromResponse(t *testing.T, body io.Reader) (apps []App) {
-	t.Helper()
-
-	err := json.NewDecoder(body).Decode(&apps)
-	if err != nil {
-		t.Fatalf("Unable to parse response %q from server into slice of Apps, '%v'", body, err)
-	}
-
-	return
-}
-
 //
 // Asserts
 //
